bde4a69ebba5d337277b67d26f099f928d30dbb5
PeterTV <challengepeter41@gmail.com>
2022-10-15 23:48:05 +0200

    save

--- game.lua
+++ game.lua
@@ -1,4 +1,87 @@
 local Game = {}
 
+lst_ennemy = {}
+
+SAUVEGARDE.score = 0
+SAUVEGARDE.life = 3 
+SAUVEGARDE.directionColor = "red"
+
+local hero = {}
+hero.img = nil
+hero.x = nil
+hero.y = nil
+hero.dim = nil
+
+local function createEnnemy(pX,pY,pColor)
+  local e = {}
+  
+  e.x = pX
+  e.y = pY
+  e.dim = 25
+  e.color = pColor
+  
+  table.insert(lst_ennemy, e)
+end
+
+function Game.load()
+  hero.x = largeur_ecran/2
+  hero.y = hauteur_ecran/2
+  hero.dim = 25
+  
+  
+  createEnnemy(200,200,"green")
+end
+
+function Game.update(dt)
+  --verifie si ça collide
+  for k,v in ipairs(lst_ennemy) do
+    if CheckCollision(hero.x,hero.y,25,25, v.x,v.y,25,25) then
+      if SAUVEGARDE.directionColor ~= v.color then
+        SAUVEGARDE.life = SAUVEGARDE.life - dt
+      else
+        SAUVEGARDE.life = SAUVEGARDE.life + dt
+        SAUVEGARDE.score = SAUVEGARDE.score + dt
+      end
+    end
+  end
+  
+  if love.keyboard.isDown("right") then
+    SAUVEGARDE.directionColor = "red" 
+    hero.x = hero.x + 150*dt
+  elseif love.keyboard.isDown("down") then
+    SAUVEGARDE.directionColor = "blue"
+    hero.y = hero.y + 150*dt
+  elseif love.keyboard.isDown("left") then
+    SAUVEGARDE.directionColor = "yellow"
+    hero.x = hero.x - 150*dt
+  elseif love.keyboard.isDown("up") then
+    SAUVEGARDE.directionColor = "green"
+    hero.y = hero.y - 150*dt
+  end
+end
+
+function Game.draw()
+  --gui
+  love.graphics.print("score : "..SAUVEGARDE.score)
+  love.graphics.print("life : "..SAUVEGARDE.life, 0, 15)
+  love.graphics.print("pour save appuie sur S", 0, 30)
+  
+  --gameplay
+  hero.img = love.graphics.rectangle("fill", hero.x, hero.y, hero.dim, hero.dim)
+  
+  for k,v in ipairs(lst_ennemy) do
+    love.graphics.rectangle("line", v.x, v.y, v.dim, v.dim)
+  end
+end
+
+function Game.keypressed(key)
+  if key == "s" then
+    mJSON = myJSON.encode(SAUVEGARDE)
+    local file = love.filesystem.newFile("game.json")
+    file:open("w")
+    file:write(mJSON)
+    file:close()
+  end
+end
 
 return Game
--- main.lua
+++ main.lua
@@ -8,6 +8,13 @@ love.graphics.setDefaultFilter("nearest")
 -- Cette ligne permet de déboguer pas à pas dans ZeroBraneStudio
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
 SAUVEGARDE = {}
 
 local lst_buttons = {}
@@ -39,6 +46,8 @@ function love.load()
   createButtons(15, 100, "New Partie", "NP")
   createButtons(15, 200, "Load Partie", "LP")
   
+  Game.load()
+  
 end
 
 local function Menu_update(dt)
@@ -48,6 +57,8 @@ end
 function love.update(dt)
   if gameState == "menu" then
     Menu_update(dt)
+  elseif gameState == "game" then
+    Game.update(dt)
   end
 end
 
@@ -64,6 +75,8 @@ end
 function love.draw()
   if gameState == "menu" then
     Menu_draw()
+  elseif gameState == "game" then
+    Game.draw()
   end
 end
 
@@ -71,16 +84,29 @@ function love.keypressed(key)
   
   print(key)
   
+  if gameState == "game" then
+    Game.keypressed(key)
+  end
+  
 end
 
 function love.mousepressed(px, py, pbutton)
-  for k,v in ipairs(lst_buttons) do
-    if pbutton == 1 and px >= v.x and px <= v.x+v.l and py >= v.y and py <= v.y+v.h then
-      if v.ID == "NP" then
-        print("je lance une nouvelle partie")
-      end
-      if v.ID == "LP" then
-        print("je lance une partie déjà chargé")
+  if gameState == "menu" then
+    for k,v in ipairs(lst_buttons) do
+      if pbutton == 1 and px >= v.x and px <= v.x+v.l and py >= v.y and py <= v.y+v.h then
+        if v.ID == "NP" then
+          print("je lance une nouvelle partie")
+          gameState = "game"
+        end
+        if v.ID == "LP" then
+          print("je lance une partie déjà chargé")
+          local file = love.filesystem.newFile("game.json")
+          if love.filesystem.getInfo("game.json") ~= nil then
+            file:open("r")
+            SAUVEGARDE = myJSON.decode(file:read())
+            gameState = "game"
+          end
+        end
       end
     end
   end
